640dc00931ccf6344d54cccf5613314b5ce3b638
Joel Drapper <joel@drapper.me>
2022-11-24 17:45:11 +0000

    Fix Zeitwerk loader

--- build.rb
+++ build.rb
@@ -7,7 +7,7 @@ require "bundler"
 Bundler.require :default
 
 loader = Zeitwerk::Loader.new
-loader.push_dir(__dir__)
+loader.push_dir("#{__dir__}")
 loader.enable_reloading
 loader.setup
 
@@ -18,7 +18,6 @@ PageBuilder.build_all
 if ARGV.include? "--watch"
 	Filewatcher.new(["#{__dir__}/**/*rb", "#{__dir__}/**/*md"]).watch do |_changes|
 		loader.reload
-		loader.eager_load
 		PageBuilder.build_all
 	end
 end
