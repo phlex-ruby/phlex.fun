8f73ad22e9d0a351a18827f1f2a312fc59b7c5e8
Joel Drapper <joel@drapper.me>
2022-11-24 17:45:17 +0000

    Update nav

--- components/nav.rb
+++ components/nav.rb
@@ -7,9 +7,33 @@ module Components
 			label for: "nav-toggle", class: "top-0 bottom-0 left-0 right-0 bg-gray-900 opacity-0 peer-checked:fixed peer-checked:z-30 peer-checked:block peer-checked:opacity-50 lg:hidden lg:peer-checked:hidden"
 
 			nav do
-				views
-				testing
-				rails
+				render Nav::Group.new("") do |g|
+					g.item("Introduction", to: "/")
+					g.item("Setup", to: "/setup/")
+				end
+
+				render Nav::Group.new("HTML") do |g|
+					g.item("Introduction", to: "/html/introduction/")
+					g.item("Tags", to: "/html/tags/")
+					g.item("Helpers", to: "/html/helpers/")
+				end
+
+				render Nav::Group.new("Testing") do |g|
+					g.item("Introduction", to: "/testing/")
+					g.item("Nokogiri", to: "/testing/nokogiri/")
+					g.item("Capybara", to: "/testing/capybara/")
+					g.item("Rails", to: "/rails/testing/")
+				end
+
+				render Nav::Group.new("Rails") do |g|
+					g.item("Introduction", to: "/rails/")
+					g.item("Generators", to: "/rails/generators/")
+					g.item("Rendering views", to: "/rails/rendering-views/")
+					g.item("Layouts", to: "/rails/layouts/")
+					g.item("Helpers", to: "/rails/helpers/")
+					g.item("Migrating to Phlex", to: "/rails/migrating/")
+					g.item("Testing", to: "/rails/testing/")
+				end
 			end
 		end
 
@@ -17,35 +41,6 @@ module Components
 
 		def nav
 			super(class: "fixed lg:relative w-3/4 border-r-2 border-gray-100 lg:border-0 lg:w-1/4 text-lg lg:text-base h-full z-40 px-10 py-5 -left-full peer-checked:left-0 lg:left-0 bg-white")
-		end
-
-		def views
-			render Nav::Group.new("HTML") do |g|
-				g.item("Introduction", to: "/html/introduction/")
-				g.item("Tags", to: "/html/tags/")
-				g.item("Helpers", to: "/html/helpers/")
-			end
-		end
-
-		def testing
-			render Nav::Group.new("Testing") do |g|
-				g.item("Introduction", to: "/testing/")
-				g.item("Nokogiri", to: "/testing/nokogiri/")
-				g.item("Capybara", to: "/testing/capybara/")
-				g.item("Rails", to: "/rails/testing/")
-			end
-		end
-
-		def rails
-			render Nav::Group.new("Rails") do |g|
-				g.item("Introduction", to: "/rails/")
-				g.item("Generators", to: "/rails/generators/")
-				g.item("Rendering views", to: "/rails/rendering-views/")
-				g.item("Layouts", to: "/rails/layouts/")
-				g.item("Helpers", to: "/rails/helpers/")
-				g.item("Migrating to Phlex", to: "/rails/migrating/")
-				g.item("Testing", to: "/rails/testing/")
-			end
 		end
 	end
 end
