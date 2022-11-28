9ef293b846981122432713f37d49d538840e2ae8
Bradley Schaefer <bradley.schaefer@gmail.com>
2022-11-28 17:00:59 -0500

    Generate new icon images from SVG source
    This adds a few larger-sized icons, made possible by exporting
    different-sized images from the SVG source. The previous source image
    was too small and upscaling looked bad, so was not possible.
    
    Given the bigger icons, updated the places that need to know about the
    new sizes.
    
    PNGs exported from SVG using Inkscape 1.2.1 (9c6d41e4, 2022-07-14)

--- components/layout.rb
+++ components/layout.rb
@@ -17,12 +17,12 @@ module Components
 					meta name: "viewport", content: "width=device-width, initial-scale=1"
 
 					# favicon
-					# generated via https://realfavicongenerator.net/ on 2022-11-25
-					link rel: "apple-touch-icon", sizes: "152x152", href: "/assets/favicon/apple-touch-icon.png"
+					# generated via https://realfavicongenerator.net/ on 2022-11-28
+					link rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicon/apple-touch-icon.png"
 					link rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicon/favicon-32x32.png"
 					link rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicon/favicon-16x16.png"
 					link rel: "manifest", href: "/assets/favicon/site.webmanifest"
-					link rel: "mask-icon", href: "/assets/favicon/safari-pinned-tab.svg", color: "#5bbad5"
+					link rel: "mask-icon", href: "/assets/favicon/safari-pinned-tab.svg", color: "#c90058"
 					link rel: "shortcut icon", href: "/assets/favicon/favicon.ico"
 					meta name: "msapplication-TileColor", content: "#00aba9"
 					meta name: "msapplication-config", content: "/assets/favicon/browserconfig.xml"
