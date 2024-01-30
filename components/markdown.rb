3946944279b82bb0f03ae985b3de157e2c07d868
Will Cosgrove <will@cosgrove.email>
2024-01-29 23:07:43 -0600

    Add phlex and phlexecute markdown code blocks
    `phlex` code blocks will be `eval`ed in an execution context scoped to
    the markdown file, and the code that was `eval`ed will be rendered as
    a Ruby code block.
    
    `phlexecute` code blocks will also be `eval`ed in the same execution
    context, scoped to the markdown file, but the resulting output of the
    `eval` will be rendered as an HTML code block.

--- components/markdown.rb
+++ components/markdown.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
+require "phlex/testing/view_helper"
 
 module Components
 	class Markdown < Phlex::Markdown
 		def code_block(code, language)
-			if language == "phlex"
-				instance_eval(code)
+			case language
+			when "phlex"
+				eval_code(code)
+				render CodeBlock.new(code, syntax: "ruby")
+			when "phlexecute"
+				output = eval_code(code)
+				pretty = HtmlBeautifier.beautify(output)
+				render CodeBlock.new(pretty, syntax: "html")
 			else
 				render CodeBlock.new(code, syntax: language)
+			end
+		end
+
+		private
+
+		def eval_code(code)
+			eval_context.module_eval(code)
+		end
+
+		def eval_context
+			@eval_context ||= Module.new do
+				extend Phlex::Testing::ViewHelper
 			end
 		end
 	end
